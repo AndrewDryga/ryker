1dfc3a989b8af889c9605e9547003e072113527f
Andrew Dryga <andrew@dryga.com>
2026-08-30 23:35:02 -0600

    Use current Mix release sequencing

--- test/responder/release_test.exs
+++ test/responder/release_test.exs
@@ -48,7 +48,7 @@ defmodule Responder.ReleaseTest do
     assert makefile =~ "scripts/check-elixir-candidate.sh"
     assert makefile =~ "scripts/elixir-release-version.sh"
     assert makefile =~ "RESPONDER_ELIXIR_VERSION="
-    assert makefile =~ "scripts/elixir-mix.sh do clean, release responder --overwrite"
+    assert makefile =~ "scripts/elixir-mix.sh do clean + release responder --overwrite"
 
     version_script = File.read!(Path.expand("../../scripts/elixir-release-version.sh", __DIR__))
     assert version_script =~ "describe --exact-match --tags"
